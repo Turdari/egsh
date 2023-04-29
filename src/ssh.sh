8335b2f7dd81a6aa194658e78194e7aaf9535c70
tmp <tmp@tmp.com>
2023-04-29 12:40:24 +0900

    [ADD] 1: vnc, 2: fw , 3: kbuild, 4: lab, 5: web

--- src/ssh.sh
+++ src/ssh.sh
@@ -2,7 +2,7 @@
 
 
 #set server ssh tunnel in client 
-ssh_tunnel_client_set_vnc()
+ssh_tunnel_pc1_p5901()
 {
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
 
@@ -12,16 +12,53 @@ ssh_tunnel_client_set_vnc()
 #	while the -N switch tells ssh that we don’t want to execute a remote command. 
 #	The -l switch specifies the remote login name.
 
-	[ ! -z $1 ] && [ ! -z $2 ] && ssh -L 5901:127.0.0.1:5901 -C -N -l $1 $2
+	[ ! -z $1 ] && [ ! -z $2 ] && ssh -p 2201 -L 5901:127.0.0.1:5901 -C -N -l $1 $2
 	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
 	return
 }
 
-#ssh_tunnel_client_unset_vnc()
-#{
-#	
-#	return
-#}
+ssh_tunnel_pc3_p5900()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
 
+#	The -L switch specifies the port bindings. 
+#	In this case we’re binding port 5901 of the remote connection to port 5901 on your local machine. 
+#	The -C switch enables compression, 
+#	while the -N switch tells ssh that we don’t want to execute a remote command. 
+#	The -l switch specifies the remote login name.
 
+	[ ! -z $1 ] && [ ! -z $2 ] && ssh -p 2203 -L 5900:127.0.0.1:5900 -C -N -l $1 $2
+	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
+	return
+}
+
+ssh_tunnel_pc4_p5900()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
+
+#	The -L switch specifies the port bindings. 
+#	In this case we’re binding port 5901 of the remote connection to port 5901 on your local machine. 
+#	The -C switch enables compression, 
+#	while the -N switch tells ssh that we don’t want to execute a remote command. 
+#	The -l switch specifies the remote login name.
+
+	[ ! -z $1 ] && [ ! -z $2 ] && ssh -p 2204 -L 5900:127.0.0.1:5900 -C -N -l $1 $2
+	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
+	return
+}
+
+ssh_tunnel_pc5_p5900()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
+
+#	The -L switch specifies the port bindings. 
+#	In this case we’re binding port 5901 of the remote connection to port 5901 on your local machine. 
+#	The -C switch enables compression, 
+#	while the -N switch tells ssh that we don’t want to execute a remote command. 
+#	The -l switch specifies the remote login name.
+
+	[ ! -z $1 ] && [ ! -z $2 ] && ssh -p 2205 -L 5900:127.0.0.1:5900 -C -N -l $1 $2
+	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
+	return
+}
 
