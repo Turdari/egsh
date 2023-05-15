bc2c396051ecc065bfb135f9d7d863c957cb9714
Geunhyeuk LEE <turi@turiwin>
2023-05-15 13:59:59 +0900

    [CHG] stage 1: change ~/.egsh location

--- src/ssh.sh
+++ src/ssh.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 
+ssh_tunnel()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 > <port_range>"
+
+    return
+}
+
+ssh_reverse_tunnel()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 > <port_range>"
+
+    return
+}
+
+sshx_app()
+{
+	echo "${FUNCNAME[0]} < usr > < server ipv4 > <port> <app-name>"
+
+    [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && [ -z $4 ]
+
+
+}
+
 #set server ssh tunnel in client 
 ssh_tunnel_pc1_p5901()
 {
@@ -16,7 +39,6 @@ ssh_tunnel_pc1_p5901()
 	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
 	return
 }
-
 ssh_tunnel_pc3_p5900()
 {
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
@@ -31,7 +53,6 @@ ssh_tunnel_pc3_p5900()
 	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
 	return
 }
-
 ssh_tunnel_pc4_p5900()
 {
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
@@ -46,7 +67,6 @@ ssh_tunnel_pc4_p5900()
 	#ssh -L 127.0.0.1:5901:127.0.0.1:5901 -C -N -l $1 $2
 	return
 }
-
 ssh_tunnel_pc5_p5900()
 {
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
@@ -67,14 +87,11 @@ sshx_pc4()
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
     [ ! -z $1 ] && [ ! -z $2 ] && ssh -X -p 2204 $1@$2
 }
-
 sshx_pc5()
 {
 	echo "${FUNCNAME[0]} < usr > < server ipv4 >"
     [ ! -z $1 ] && [ ! -z $2 ] && ssh -X -p 2205 $1@$2
 }
-
-
 
 
 
