a7969fb798e6417826734ca3c47cbd8d268c0193
Geunhyeuk LEE <turi@turiwin>
2023-05-15 10:24:15 +0900

    [CHG] egsh directory changed to ~/.local/share and ~/.local/bin

--- lib/DCMD/install.sh
+++ lib/DCMD/install.sh
@@ -11,13 +11,30 @@ export EGSH_CTX_ESYNC_LIB ;
 export EGSH_CTX_DCMD_LIB ;
 export EGSH_CTX_PKG_LIB ;
 
+#Fixme
+# Only in case when .local/bin is not in the environment 
+# but it can cause error. so should be fixed later
+bashrc_file="$HOME/.bashrc"
+bin_path="$HOME/.local/bin"
+if [[ ":$PATH:" != *":$bin_path:"* ]]; then
+    echo "Adding $bin_path to PATH in $bashrc_file"
+    echo "##EGSH install##" >> "$bashrc_file"
+    echo "export PATH=\$PATH:$bin_path" >> "$bashrc_file"
+else
+    echo "$bin_path is already in PATH"
+fi
 
-INS_DIR_LIB=/usr/local/lib
-INS_DIR_BIN=/usr/local/bin
+#INS_DIR_LIB=/usr/local/lib
+#INS_DIR_BIN=/usr/local/bin
+INS_DIR_LIB=$HOME/.local/share
+INS_DIR_BIN=$HOME/.local/bin
 
-sudo rm -rf $INS_DIR_LIB/egsh
-sudo rm -rf $INS_DIR_BIN/egsh
+# check directory
+mkdir -p "$INS_DIR_BIN" 
 
-sudo git clone $EGSH_DIR $INS_DIR_LIB/egsh
-sudo git -C $INS_DIR_LIB/egsh remote set-url origin https://github.com/Turdari/egsh.git
-sudo ln -s $INS_DIR_LIB/egsh/egsh $INS_DIR_BIN
+rm -rf $INS_DIR_LIB/egsh
+rm -rf $INS_DIR_BIN/egsh
+
+git clone $EGSH_DIR $INS_DIR_LIB/egsh
+git -C $INS_DIR_LIB/egsh remote set-url origin https://github.com/Turdari/egsh.git
+ln -s $INS_DIR_LIB/egsh/egsh $INS_DIR_BIN/egsh
