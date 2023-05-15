8928ac1466ea068ca4f10aa987d7a5683854aeb6
Geunhyeuk LEE <turi@turiwin>
2023-05-15 11:26:23 +0900

    [ADD] added git update scheme

---
+++ lib/DCMD/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export EGSH_DIR ;
+export EGSH_VERSION ;
+export EGSH_USR_DIR ;
+export EGSH_USR_INC ;
+export EGSH_USR_ESYNC ;
+export EGSH_USR_DCMD ;
+export EGSH_CTX_SRC ;
+export EGSH_CTX_ESYNC ;
+export EGSH_CTX_ESYNC_LIB ;    
+export EGSH_CTX_DCMD_LIB ;
+export EGSH_CTX_PKG_LIB ;
+
+git -C $EGSH_DIR pull origin
+
