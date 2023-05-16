d6b19341964979e7b4eda18a3088e78ba9104357
Geunhyeuk LEE <turi@turiwin>
2023-05-16 09:49:20 +0900

    [ADD] added remove feature, pretest

--- lib/DCMD/remove.sh
+++ lib/DCMD/remove.sh
@@ -4,10 +4,9 @@
 echo "<EGSH remove.sh>"
 #echo "script location : $0"
 #echo "target location : $1"
-
+sleep 1
 ARGS=( $@  )
-echo ${ARGS[@]}
+echo "remove list : ${ARGS[@]}"
+sudo rm -rf ${ARGS[@]}
 #rm -rf $INS_DIR_LIB/egsh
-#rm -rf $INS_DIR_LIB/egsh
-
-
+echo "DONE!"
